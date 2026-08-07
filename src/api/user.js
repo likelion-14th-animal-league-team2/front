4a6f2d881f5079d72d37d50b1ed83d9fa8e8cf8b
- <moonjw727@gmail.com>
2026-08-07 18:22:56 +0900

    chore(#5): 로컬 데모용 목데이터 추가 (프로필/문서함/지원내역)

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,13 @@
-import axiosInstance from "./axiosInstance";
+// TODO: 백엔드 팀 API 연동되면 아래 mock 제거하고 axiosInstance.get("/users/me") 호출로 교체
+const MOCK_PROFILE = {
+  name: "김레주",
+  email: "김레주@email.com",
+  membership: "멤버",
+  age: 26,
+  country: "대한민국",
+  avatarUrl: null,
+};
 
 export const getMyProfile = async () => {
-  const { data } = await axiosInstance.get("/users/me");
-  return data; // { name, email, membership, age, country, avatarUrl }
+  return MOCK_PROFILE; // { name, email, membership, age, country, avatarUrl }
 };
