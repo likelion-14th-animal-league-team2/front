cdf7a90890683ba1f5b70c20a1c2c289c3b0a159
bomikim0214 <rlaqhals05@naver.com>
2026-08-18 13:05:26 +0900

    feat(api): 카카오로그인 환경변수 추가, 백엔드와 연결

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,5 +1,8 @@
 import { IconLogo, IconChatBubble } from "../../components/common/icons";
 
+const KAKAO_CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
+const KAKAO_REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
 function LoginPage() {
   const handleKakaoLogin = () => {
